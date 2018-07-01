eae37140503cd4a7eba435b20ac5ea4b0d19ad3f
Newana <newana.tandukar@gmail.com>
2018-07-01 22:09:35 +0545

    Done

---
+++ app/src/main/java/com/example/newana/samachar/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.newana.samachar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loads default fragment
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_categories:
+                    fragment = new CategoriesFragment();
+                    break;
+                case R.id.navigation_favorites:
+                    fragment = new FavoritesFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/newana/samachar/SingleCategoryView.java
@@ -0,0 +1,16 @@
+package com.example.newana.samachar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SingleCategoryView extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_view);
+
+
+    }
+}
